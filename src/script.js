400c28bafa10dd9839aa356bb6e2522bd75c87bc
BrandonImmersive <brandon@immersivevr.co.uk>
2021-11-12 17:27:31 +0000

    added astronaut, went back to threejs r133 to fix removal

--- src/script.js
+++ src/script.js
@@ -2,18 +2,25 @@ import './style.css'
 import * as THREE from 'three'
 import * as dat from 'lil-gui'
 import * as ZapparThree from "@zappar/zappar-threejs";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 /**
  * Base
  */
+
+const placeButton = document.getElementById("tap-to-place") || document.createElement("div")
+
 // Debug
 const gui = new dat.GUI()
+let manager = new ZapparThree.LoadingManager();
+const gltfLoader = new GLTFLoader(manager)
 
 // Renderer
 const renderer = new THREE.WebGLRenderer()
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setPixelRatio(window.devicePixelRatio)
 document.body.appendChild(renderer.domElement)
 renderer.setSize(window.innerWidth, window.innerHeight)
+
 window.addEventListener("resize", () => {
     renderer.setSize(window.innerWidth, window.innerHeight)
 });
@@ -35,11 +42,40 @@ ZapparThree.permissionRequestUI().then(granted => {
     else ZapparThree.permissionDeniedUI();
 });
 
+const instantTracker = new ZapparThree.InstantWorldTracker();
+const instantTrackerGroup = new ZapparThree.InstantWorldAnchorGroup(
+  camera,
+  instantTracker
+)
+
+gltfLoader.load(
+    'models/astronaut.glb',
+    (gltf) =>
+    {
+        gltf.scene.scale.set(0.5,0.5,0.5)
+        instantTrackerGroup.add(gltf.scene)
+    }
+)
+
+scene.add(instantTrackerGroup)
+
+placeButton.style.display = "block"
+
+let hasPlaced = false;
+placeButton.addEventListener("click", () => {
+    hasPlaced = true
+    placeButton.remove();
+});
+
 // Animate
 const clock = new THREE.Clock()
 
 const tick = () =>
 {
+    if (!hasPlaced) {
+        instantTrackerGroup.setAnchorPoseFromCameraOffset(0, 0, -4);
+    }
+
     const elapsedTime = clock.getElapsedTime()
 
     // Render
